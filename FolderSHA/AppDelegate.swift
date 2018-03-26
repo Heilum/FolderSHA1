325d57797d52c6ff3f73153f05c7ecb3a9b3a5b4
Jagie <chen_cwf@163.com>
2018-03-26 11:41:29 +0800

    Initial Commit

---
+++ FolderSHA/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FolderSHA
+//
+//  Created by CHENWANFEI on 26/03/2018.
+//  Copyright © 2018 swordfish. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FolderSHA/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FolderSHA
+//
+//  Created by CHENWANFEI on 26/03/2018.
+//  Copyright © 2018 swordfish. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
